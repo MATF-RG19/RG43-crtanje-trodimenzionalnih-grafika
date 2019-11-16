6ab5de1a61991a4bdb55f0537795e9dac4ce645d
Robotmurlock <momir.adzemovic@gmail.com>
2019-11-16 23:07:10 +0100

    Reorganizing files

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+#include <ncurses.h>
+
+#include "utility.cpp"
+#include "colors.h"
+#include "draw.cpp"
+#include "camera.cpp"
+
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_display(void);
+
+Camera camera(utility::Vector(1, 1, 1), utility::Vector(0, 0, 0), utility::Vector(0, 1, 0));
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+
+    glutDisplayFunc(on_display);
+    glutKeyboardFunc(on_keyboard);
+
+    glClearColor(GRAY, 0);
+    glutMainLoop();
+
+    return 0;
+}
+static void on_keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    
+    case 'w': //UP
+        camera.rotate_up();
+        on_display();
+        break;
+    case 's': //DOWN
+        camera.rotate_down();
+        on_display();
+        break;
+    case 'd': //RIGHT
+        camera.rotate_right();
+        on_display();
+        break;
+    case 'a': //LEFT
+        camera.rotate_left();
+        on_display();
+        break;
+    case 'z':
+        camera.zoom_in();
+        on_display();
+        break;
+    case 'x':
+        camera.zoom_out();
+        on_display();
+        break;
+    }
+}
+
+static void on_display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    camera.update();
+    draw_axis();
+    draw_grid();
+
+    glutSwapBuffers();
+}
+
